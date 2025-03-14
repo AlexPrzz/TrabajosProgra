e13cc46bc1ea3139044d37645d7800c322c20805
AlexPrzz <aleperper11@alu.edu.gva.es>
2025-03-14 08:41:39 +0100

    Delete Luna.java

--- Luna.java
+++
@@ -1,8 +0,0 @@
-public class Luna extends CuerpoCeleste{
-
-    public Luna(String nombre, double periodoOrbital, TipoCuerpoCeleste tipoCuerpo) {
-        super(nombre, periodoOrbital, tipoCuerpo);
-    }
-
-
-}

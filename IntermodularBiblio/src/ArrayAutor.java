c265c3445335f357832bcd382d47925eddbf582a
AlexPrzz <aleperper11@alu.edu.gva.es>
2025-03-20 13:52:15 +0100

    Add files via upload

---
+++ IntermodularBiblio/src/ArrayAutor.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+
+public class ArrayAutor {
+    private ArrayList<Autor> autores;
+
+    public ArrayAutor() {
+        this.autores = new ArrayList<>();
+        // Datos de ejemplo
+        autores.add(new Autor(1, "Española", "Premio Cervantes", "Miguel", "de Cervantes", "Saavedra"));
+        autores.add(new Autor(2, "Británica", "Premio Nobel", "William", "Shakespeare", ""));
+        autores.add(new Autor(3, "Rusa", "Premio Pushkin", "Aleksandr", "Púshkin", ""));
+    }
+
+    public boolean addNewAutor(Autor autor) {
+        if (findAutorById(autor.getIdAutor()) != null) {
+            return false;
+        }
+        autores.add(autor);
+        return true;
+    }
+
+    public boolean removeAutor(int idAutor) {
+        Autor autor = findAutorById(idAutor);
+        if (autor != null) {
+            autores.remove(autor);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean updateAutor(Autor autor) {
+        Autor existingAutor = findAutorById(autor.getIdAutor());
+        if (existingAutor != null) {
+            int index = autores.indexOf(existingAutor);
+            autores.set(index, autor);
+            return true;
+        }
+        return false;
+    }
+
+    public Autor findAutorById(int idAutor) {
+        for (Autor autor : autores) {
+            if (autor.getIdAutor() == idAutor) {
+                return autor;
+            }
+        }
+        return null;
+    }
+
+    public void printAutor() {
+        for (Autor autor : autores) {
+            autor.mostrarInformacion();
+            //System.out.println("-----------");
+        }
+    }
+}
---
+++ IntermodularBiblio/src/Autor.java
@@ -0,0 +1,93 @@
+public class Autor {
+    private int idAutor;
+    private String nacionalidad;
+    private String premios;
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+
+    // Constructor
+    public Autor(int idAutor, String nacionalidad, String premios, String nombre, String apellido1, String apellido2) {
+        if (idAutor <= 0) {
+            throw new IllegalArgumentException("El ID del autor no puede ser cero o negativo.");
+        }
+        this.idAutor = idAutor;
+        this.nacionalidad = nacionalidad;
+        this.premios = premios;
+        if (nombre.isEmpty()) {
+            this.nombre = "Anónimo";
+        } else {
+            this.nombre = nombre;
+        }
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+    }
+
+    // Getters y Setters
+    public int getIdAutor() {
+        return idAutor;
+    }
+
+    public void setIdAutor(int idAutor) {
+        if (idAutor <= 0) {
+            throw new IllegalArgumentException("El ID del autor no puede ser cero o negativo.");
+        }
+        this.idAutor = idAutor;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getPremios() {
+        return premios;
+    }
+
+    public void setPremios(String premios) {
+        this.premios = premios;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        if (nombre.isEmpty()) {
+            this.nombre = "Anónimo";
+        } else {
+            this.nombre = nombre;
+        }
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    // Método para mostrar la información en formato XML
+    public void mostrarInformacion() {
+        System.out.println("  <Autor>");
+        System.out.println("    <ID>" + idAutor + "</ID>");
+        System.out.println("    <Nacionalidad>" + nacionalidad + "</Nacionalidad>");
+        System.out.println("    <Premios>" + premios + "</Premios>");
+        System.out.println("    <Nombre>" + nombre + "</Nombre>");
+        System.out.println("    <Apellido1>" + apellido1 + "</Apellido1>");
+        System.out.println("    <Apellido2>" + apellido2 + "</Apellido2>");
+        System.out.println("  </Autor>");
+    }
+}
